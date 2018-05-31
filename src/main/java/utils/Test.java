1558bd457a2ba4f8caa839962112da58600ea930
duonganh2812 <duong@DESKTOP-BN6KK9A>
2018-05-31 18:45:00 +0700

    update

--- src/main/java/utils/Test.java
+++ src/main/java/utils/Test.java
@@ -1,8 +1,15 @@
 package utils;
 
+import java.util.Date;
+
 public class Test {
 
 	public static void main(String[] args) {
-		
+		int total = 181;
+		String link = "https://box.load.la/ui2/%s/iuI/manga/17/Original_RewriteClinic/0";
+		for (int i = 0; i < total; i++) {
+			System.out.println(String.format(link, new Date().getTime() / 1000) + i);
+		}
+
 	}
 }
